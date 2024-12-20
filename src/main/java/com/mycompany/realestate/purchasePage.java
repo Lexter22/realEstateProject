8644e03d3b2850d64adfd8edcc974769592a2008
ASYYves <yvesbullet.carranza29@gmail.com>
2024-12-20 22:25:17 +0800

    Merge origin/Carranza into Carranza
    # Conflicts:
    #	src/main/java/com/mycompany/realestate/ClientInterface.java
    #	src/main/java/com/mycompany/realestate/ClientInterfaceMain.java

--- src/main/java/com/mycompany/realestate/purchasePage.java
+++ src/main/java/com/mycompany/realestate/purchasePage.java
@@ -4,11 +4,16 @@
  */
 package com.mycompany.realestate;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -27,8 +32,10 @@ public class purchasePage extends JFrame implements ActionListener{
     private ImageIcon logoIc, finalLogoIc;
     private String username, fname, lname, userNum, userEmail;
     private int userId;
+    private Connection con;
         
     purchasePage(){
+        
         setTitle("Purchase Form");
         setSize(400, 600);
         setLayout(null);
@@ -57,15 +64,15 @@ public class purchasePage extends JFrame implements ActionListener{
         add(lbPurchaseForm);
 
         lblId = new JLabel("ID:");
-        lblId.setBounds(30, 260, 70, 40);
+        lblId.setBounds(30, 260, 200, 40);
         add(lblId);
         
         lblLocation = new JLabel("Location:");
-        lblLocation.setBounds(30, 320, 70, 40);
+        lblLocation.setBounds(30, 320, 200, 40);
         add(lblLocation);
         
         lblDate = new JLabel("Date:");
-        lblDate.setBounds(30, 390, 70, 40);
+        lblDate.setBounds(30, 390, 200, 40);
         add(lblDate);
         
         btnBack = new JButton("Back");
@@ -78,9 +85,55 @@ public class purchasePage extends JFrame implements ActionListener{
         
         btnBack.addActionListener(this);
         btnContinue.addActionListener(this);
+        databaseGetInfo();
         
     }
 
+    private void databaseGetInfo() {
+        
+        String url = "jdbc:mysql://localhost:3306/realestates";
+        String username = "root";
+        String password = "admin123";
+        
+         String getInfoForConfirm = "SELECT id, location FROM residentialrealestates";
+
+         
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+         PreparedStatement ps = connection.prepareStatement(getInfoForConfirm);
+         ResultSet rs = ps.executeQuery()) {
+        
+
+        if (rs.next()) { 
+            
+            
+            if (rs.next()) { 
+ 
+                
+                lblId.setText("ID:     " + rs.getString("id"));
+                lblLocation.setText("Location:   " + rs.getString("location"));
+            } 
+            
+            else {
+                
+                
+                lblId.setText("ID:     Not Found");
+                lblLocation.setText("Location:   Not Found");
+                
+                
+            }
+            
+            
+        }
+
+        
+    } 
+        
+        catch (Exception e) {
+            
+        Logger.getLogger(purchasePage.class.getName()).log(Level.SEVERE, null, e);
+    }
+}
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == btnContinue){
@@ -95,4 +148,6 @@ public class purchasePage extends JFrame implements ActionListener{
             dispose();
         }
     }
+
+    
 }
